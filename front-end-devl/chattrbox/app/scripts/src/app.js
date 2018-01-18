7ce815b41977c6d852fa7959359a99353cd41f5f
John Wu <john.z.wu@gmail.com>
2018-01-18 10:16:01 -0500

    crypto session storage

--- front-end-devl/chattrbox/app/scripts/src/app.js
+++ front-end-devl/chattrbox/app/scripts/src/app.js
@@ -1,15 +1,26 @@
 import socket from './ws-client';
-import {ChatForm, ChatList} from "./dom";
+import {ChatForm, ChatList, promptUsername} from "./dom";
+import {UserStore} from './storage';
+
 
 const FORM_SELECTOR='[data-chat="chat-form"]';
 const INPUT_SELECTOR='[data-chat="message-input"]';
 const LIST_SELECTOR='[data-chat="message-list"]';
 
+//let username = '';
+let userStore = new UserStore('x-chattrbox/u');
+let username = userStore.get();
+if (!username) {
+  username = promptUsername();
+  userStore.set(username);
+}
+
+
 class ChatApp {
   constructor(){
     console.log('Hello ES6');
     this.chatForm  = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
-    this.chatList  = new ChatList(LIST_SELECTOR, 'snow');
+    this.chatList  = new ChatList(LIST_SELECTOR, username);
 
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(()=>{
@@ -17,7 +28,8 @@ class ChatApp {
         console.log('=====register openhandler init anonymos call ' + text);
         let message = new ChatMessage({ message: text });
         socket.sendMessage(message.serialize());
-      })
+      });
+      this.chatList.init();
 
       //let message = new ChatMessage({message:'wow', user: 'zw'});
       //socket.sendMessage(message.serialize());
@@ -33,7 +45,7 @@ class ChatApp {
 class ChatMessage{
   constructor({
     message: m,
-    user: u='anonymous',
+    user: u= username,
     timestamp: t=(new Date()).getTime()
   }) {
     this.user = u;
